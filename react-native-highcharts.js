5170cf4cbf480cd53e972a1fdb2b9cd4f2a29e45
ufolux <ufolux_001@163.com>
2018-03-10 15:12:15 +0800

    Update react-native-highcharts.js

--- react-native-highcharts.js
+++ react-native-highcharts.js
@@ -1,57 +1,51 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    WebView,
-    Image,
-    Dimensions
-} from 'react-native';
+import { StyleSheet, View, WebView } from 'react-native'
 
-const win = Dimensions.get('window');
 class ChartView extends Component {
-    static ConstructMethod = {
-        STOCK_CHART: 'stockChart',
-        CHART: 'chart'
-    }
+  static ConstructMethod = {
+    STOCK_CHART: 'stockChart',
+    CHART: 'chart'
+  }
 
-    static propTypes = {
-        chartData: PropTypes.object,
-        config: PropTypes.object.isRequired,
-        options: PropTypes.object,
-        baseUri: PropTypes.string.isRequired,
-        libsUri: PropTypes.array.isRequired,
-        constructMethod: PropTypes.string.isRequired,
-        onEvent: PropTypes.func
-    }
+  static propTypes = {
+    chartData: PropTypes.object,
+    config: PropTypes.object.isRequired,
+    options: PropTypes.object,
+    baseUri: PropTypes.string.isRequired,
+    libsUri: PropTypes.array.isRequired,
+    constructMethod: PropTypes.string.isRequired,
+    onEvent: PropTypes.func
+  }
 
-    static defaultProps = {
-        chartData: {},
-        style: {},
-        config: {},
-        options: {},
-        baseUri: '',
-        libsUri: [],
-        constructMethod: 'stockChart'
-    }
+  static defaultProps = {
+    chartData: {},
+    style: {},
+    config: {},
+    options: {},
+    baseUri: '',
+    libsUri: [],
+    constructMethod: 'stockChart'
+  }
 
-    constructor(props) {
-        super(props);
+  constructor (props) {
+    super(props)
+    this.concatHTML = this._getHtml(props)
+  }
 
-        let scriptUriArr = props
-            .libsUri
-            .map(item => {
-                return `<script src="${item}"></script>`
-            })
-        let scripts = scriptUriArr.join('')
+  _getHtml (props) {
+    let scriptUriArr = props
+      .libsUri
+      .map(item => {
+        return `<script src="${item}"></script>`
+      })
+    let scripts = scriptUriArr.join('')
 
-        this.chartDataStr = this._getChartDataStr(this.props.chartData)
-        this.optionsStr = this._getOptionsStr(this.props.options)
-        this.configStr = this._getConfigStr(this.props.config)
+    this.chartDataStr = this._getChartDataStr(this.props.chartData)
+    this.optionsStr = this._getOptionsStr(this.props.options)
+    this.configStr = this._getConfigStr(this.props.config)
 
-        this.headHtml = `<html>
+    this.headHtml = `<html>
                     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
                     <style media="screen" type="text/css">
                     #container {
@@ -73,7 +67,7 @@ class ChartView extends Component {
                             window.onload = function() {
                                 Highcharts.setOptions(${this.optionsStr});
                                 Highcharts.${this.props.constructMethod}('container', `
-        this.footerHtml = `      );
+    this.footerHtml = `      );
                         }
                         </script>
                     </head>
@@ -82,148 +76,149 @@ class ChartView extends Component {
                         </div>
                     </body>
                 </html>`
+    return `${this.headHtml}${this.configStr}${this.footerHtml}`
+  }
 
-    }
-
-    _getChartDataStr(chartData) {
-        let chartDataStr = ''
-        if (chartData) {
-            for (let key in chartData) {
-                if (chartData.hasOwnProperty(key)) {
-                    const data = chartData[key]
-                    const dataStr = JSON.stringify(data, function (key, value) { //create string of json but if it detects function it uses toString()
-                        return (typeof value === 'function')
-                            ? value.toString()
-                            : value;
-                    })
-                    let newChartData = JSON.parse(dataStr)
-                    chartDataStr += `var ${key} = ${flattenObject(newChartData)}\n`
-                }
-            }
-        } 
-        return chartDataStr
-    }
-
-    _getOptionsStr(options) {
-        return JSON.stringify(options)
-    }
-
-    _getConfigStr(config) {
-        configStr = JSON.stringify(config, function (key, value) { //create string of json but if it detects function it uses toString()
+  _getChartDataStr (chartData) {
+    let chartDataStr = ''
+    if (chartData) {
+      for (let key in chartData) {
+        if (chartData.hasOwnProperty(key)) {
+          const data = chartData[key]
+          const dataStr = JSON.stringify(data, function (key, value) { // create string of json but if it detects function it uses toString()
             return (typeof value === 'function')
-                ? value.toString()
-                : value;
-        });
-        newConfig = JSON.parse(configStr)
-        return flattenObject(newConfig)
-    }
-
-    shouldComponentUpdate(nextProps) {
-        const chartDataStr = this._getChartDataStr(nextProps.chartData)
-        const configStr = this._getConfigStr(nextProps.config)
-        const optionsStr = this._getOptionsStr(nextProps.options)
-        const nextStyleStr = JSON.stringify(nextProps.style)
-        const lastStyleStr = JSON.stringify(this.props.style)
-
-        let needUpdate = false
-
-        if (chartDataStr !== this.chartDataStr) {
-            this.chartDataStr = chartDataStr
-            needUpdate = true
+              ? value.toString()
+              : value
+          })
+          let newChartData = JSON.parse(dataStr)
+          chartDataStr += `let ${key} = ${flattenObject(newChartData)}\n`
         }
+      }
+    }
+    return chartDataStr
+  }
 
-        if (configStr !== this.configStr) {
-            this.configStr = configStr
-            needUpdate = true
-        }
+  _getOptionsStr (options) {
+    return JSON.stringify(options)
+  }
 
-        if (optionsStr !== this.optionsStr) {
-            this.optionsStr = optionsStr
-            needUpdate = true
-        }
+  _getConfigStr (config) {
+    const configStr = JSON.stringify(config, function (key, value) { // create string of json but if it detects function it uses toString()
+      return (typeof value === 'function')
+        ? value.toString()
+        : value
+    })
+    const newConfig = JSON.parse(configStr)
+    return flattenObject(newConfig)
+  }
 
-        if (needUpdate) {
-            return needUpdate
-        }
+  shouldComponentUpdate (nextProps) {
+    const chartDataStr = this._getChartDataStr(nextProps.chartData)
+    const configStr = this._getConfigStr(nextProps.config)
+    const optionsStr = this._getOptionsStr(nextProps.options)
+    const nextStyleStr = JSON.stringify(nextProps.style)
+    const lastStyleStr = JSON.stringify(this.props.style)
 
-        return !(nextStyleStr === lastStyleStr && nextProps.baseUri === this.props.baseUri && nextProps.libsUri === this.props.libsUri && nextProps.constructMethod === this.props.constructMethod)
+    let needUpdate = false
+
+    if (chartDataStr !== this.chartDataStr) {
+      this.chartDataStr = chartDataStr
+      needUpdate = true
     }
 
-    // used to resize on orientation of display
-    reRenderWebView(e) {
-        this.setState({height: e.nativeEvent.layout.height, width: e.nativeEvent.layout.width})
+    if (configStr !== this.configStr) {
+      this.configStr = configStr
+      needUpdate = true
     }
 
-    render() {
-        let concatHTML = `${this.headHtml}${this.configStr}${this.footerHtml}`;
-
-        return (
-            <View style={this.props.style}>
-                <WebView
-                    onLayout={this.reRenderWebView}
-                    style={styles.full}
-                    source={{
-                    html: concatHTML,
-                    baseUrl: this.props.baseUri
-                }}
-                    javaScriptEnabled={true}
-                    domStorageEnabled={true}
-                    scalesPageToFit={true}
-                    scrollEnabled={false}
-                    onMessage={(e) => {
-                    this.props.onEvent && this
-                        .props
-                        .onEvent(e.nativeEvent.data, e)
-                }}
-                    automaticallyAdjustContentInsets={true}
-                    {...this.props}/>
-            </View>
-        );
-    };
-};
-
-var flattenObject = function (obj, str = '{') {
-    Object
-        .keys(obj)
-        .forEach(function (key) {
-            str += `${key}: ${flattenText(obj[key])}, `
-        })
-    if (str === '{') {
-        return '{}'
-    } else {
-        return `${str.slice(0, str.length - 2)}}`
+    if (optionsStr !== this.optionsStr) {
+      this.optionsStr = optionsStr
+      needUpdate = true
     }
-};
 
-var flattenText = function (item, key) {
-    if (key == "y") 
-        console.log(item, typeof item);
-    var str = ''
-    if (item && typeof item === 'object' && item.length == undefined) {
-        str += flattenObject(item)
-    } else if (item && typeof item === 'object' && item.length !== undefined) {
-        str += '['
-        item.forEach(function (k2) {
-            str += `${flattenText(k2)}, `
-        })
-        if (item.length > 0) 
-            str = str.slice(0, str.length - 2)
-        str += ']'
-    } else if (typeof item === 'string' && item.slice(0, 8) === 'function') {
-        str += `${item}`
-    } else if (typeof item === 'string') {
-        str += `\"${item.replace(/"/g, '\\"')}\"`
-    } else {
-        str += `${item}`
+    if (needUpdate) {
+      this.concatHTML = this._getHtml(nextProps)
+      return needUpdate
     }
-    return str
-};
 
-var styles = StyleSheet.create({
-    full: {
-        flex: 1,
-        backgroundColor: 'transparent'
-    }
-});
+    return !(nextStyleStr === lastStyleStr && nextProps.baseUri === this.props.baseUri && nextProps.libsUri === this.props.libsUri && nextProps.constructMethod === this.props.constructMethod)
+  }
 
-module.exports = ChartView;
+  // used to resize on orientation of display
+  reRenderWebView (e) {
+    this.setState({height: e.nativeEvent.layout.height, width: e.nativeEvent.layout.width})
+  }
+
+  render () {
+    return (
+      <View style={this.props.style}>
+        <WebView
+          onLayout={this.reRenderWebView}
+          style={styles.full}
+          source={{
+            html: this.concatHTML,
+            baseUrl: this.props.baseUri
+          }}
+          javaScriptEnabled
+          domStorageEnabled
+          scalesPageToFit
+          scrollEnabled={false}
+          onMessage={(e) => {
+            this.props.onEvent && this
+              .props
+              .onEvent(e.nativeEvent.data, e)
+          }}
+          automaticallyAdjustContentInsets
+          {...this.props} />
+      </View>
+    )
+  }
+}
+
+let flattenObject = function (obj, str = '{') {
+  Object
+    .keys(obj)
+    .forEach(function (key) {
+      str += `${key}: ${flattenText(obj[key])}, `
+    })
+  if (str === '{') {
+    return '{}'
+  } else {
+    return `${str.slice(0, str.length - 2)}}`
+  }
+}
+
+let isArray = (o) => {
+  return Object.prototype.toString.call(o) === '[object Array]'
+}
+
+let flattenText = function (item, key) {
+  if (key === 'y') { console.log(item, typeof item) }
+  let str = ''
+  if (item && typeof item === 'object' && !isArray(item)) {
+    str += flattenObject(item)
+  } else if (item && typeof item === 'object' && isArray(item)) {
+    str += '['
+    item.forEach(function (k2) {
+      str += `${flattenText(k2)}, `
+    })
+    if (item.length > 0) { str = str.slice(0, str.length - 2) }
+    str += ']'
+  } else if (typeof item === 'string' && item.slice(0, 8) === 'function') {
+    str += `${item}`
+  } else if (typeof item === 'string') {
+    str += `\"${item.replace(/"/g, '\\"')}\"`
+  } else {
+    str += `${item}`
+  }
+  return str
+}
+
+let styles = StyleSheet.create({
+  full: {
+    flex: 1,
+    backgroundColor: 'transparent'
+  }
+})
+
+module.exports = ChartView
