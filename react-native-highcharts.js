79cd8049ae4187eb7d6eaaad1b595db6b0948f84
raganyayoung <raganyayoung@webank.com>
2018-04-18 20:46:46 +0800

    添加自定义container容器大小

--- react-native-highcharts.js
+++ react-native-highcharts.js
@@ -42,6 +42,9 @@ class ChartView extends Component {
       })
     let scripts = scriptUriArr.join('')
 
+    let containerWidth = props.containerWidth ? props.containerWidth + 'px' : '100%'
+    let containerHeight = props.containerHeight ? props.containerHeight + 'px' : '100%'
+
     this.chartDataStr = this._getChartDataStr(props.chartData)
     this.optionsStr = this._getOptionsStr(props.options)
     this.configStr = this._getConfigStr(props.config)
@@ -50,8 +53,8 @@ class ChartView extends Component {
                     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
                     <style media="screen" type="text/css">
                     #container {
-                        width:100%;
-                        height:100%;
+                        width: ${containerWidth};
+                        height: ${containerHeight};
                         top:0;
                         left:0;
                         right:0;
@@ -193,7 +196,7 @@ let flattenObject = function (obj, str = '{') {
       str += `'${key}': ${flattenText(obj[key])}, `
     })
   }
-  
+
   if (str === '{') {
     return '{}'
   } else {
